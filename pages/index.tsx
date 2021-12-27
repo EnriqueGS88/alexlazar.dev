631b61c9a633e020b3328208eb77cf6c89ea9dcf
lazaralex98 <alex2012ro@gmail.com>
2021-12-27 20:51:29 +0200

    article hover effect & image is link

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,11 +98,13 @@ export default function Home() {
               >
                 {/* <div className="bg-yellow-200 relative top-4 left-4 h-full w-full"></div> */}
                 <div className="flex-shrink-0">
-                  <img
-                    className="h-48 w-full object-cover"
-                    src={post.imageUrl}
-                    alt=""
-                  />
+                  <a href={post.category.href}>
+                    <img
+                      className="h-48 w-full object-cover"
+                      src={post.imageUrl}
+                      alt=""
+                    />
+                  </a>
                 </div>
                 <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                   <div className="flex-1">
@@ -114,7 +116,7 @@ export default function Home() {
                         {post.category.name}
                       </a>
                     </p>
-                    <a href={post.href} className="block mt-2">
+                    <a href={post.href} className="block mt-2 hover:opacity-60">
                       <p className="text-xl font-semibold text-gray-900">
                         {post.title}
                       </p>
