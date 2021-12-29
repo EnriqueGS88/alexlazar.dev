d6f5030400e55268306080b1e8ceb2d8cc426cb6
lazaralex98 <alex2012ro@gmail.com>
2021-12-29 04:53:50 +0200

    dark mode for error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -21,9 +21,9 @@ export default function Error() {
           imageUrl="https://alexlazar.dev/images/vercel.svg"
         />
       </Head>
-      <div className="bg-white min-h-full flex flex-col lg:relative">
+      <div className="bg-white dark:bg-gray-900 dark:text-white min-h-full flex flex-col lg:relative">
         <div className="flex-grow flex flex-col lg:border-2 lg:border-black">
-          <main className="flex-grow flex flex-col bg-white">
+          <main className="flex-grow flex flex-col">
             <div className="flex-grow mx-auto max-w-7xl w-full flex flex-col px-4 sm:px-6 lg:px-8">
               <div className="flex-shrink-0 pt-10 sm:pt-16">
                 <Link href="/">
@@ -31,15 +31,15 @@ export default function Error() {
                 </Link>
               </div>
               <div className="flex-shrink-0 my-auto py-16 sm:py-32">
-                <p className="text-sm font-semibold text-gray-500 uppercase tracking-wide">
+                <p className="text-sm font-semibold text-gray-500 dark:text-gray-300 uppercase tracking-wide">
                   404 error
                 </p>
-                <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                <h1 className="mt-2 text-4xl font-extrabold text-gray-900 dark:text-gray-50 tracking-tight sm:text-5xl">
                   Page not found
                 </h1>
                 <div className="mt-6">
                   <Link href="/">
-                    <a className="text-base font-medium text-black hover:underline">
+                    <a className="text-base font-medium text-black dark:text-white hover:underline underline-offset-4">
                       Go back home<span aria-hidden="true"> &rarr;</span>
                     </a>
                   </Link>
