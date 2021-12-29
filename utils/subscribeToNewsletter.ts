8f1069e371a2c7bae317b12f0bfdd664ab741632
lazaralex98 <alex2012ro@gmail.com>
2021-12-29 04:07:34 +0200

    still can't quite figure out how to parse .md files

---
+++ utils/subscribeToNewsletter.ts
@@ -0,0 +1,23 @@
+export default async function subscribeToNewsletter(email) {
+  let headersList = {
+    Accept: "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.io)",
+    "Content-Type": "application/json; charset=utf-8",
+  };
+
+  let reqOptions = JSON.stringify({
+    url: "https://api.convertkit.com/v3/forms/2871455/subscribe",
+    method: "POST",
+    headers: headersList,
+    data:
+      '{ \n    "api_key": "NeIvJhiL4Cv-vr1w3_XIvg",\n    "email": "' +
+      email +
+      '"\n}',
+  });
+
+  // TODO integrate form with Convertkit
+
+  // axios.request(reqOptions).then(function (response) {
+  //   console.log(response.data);
+  // });
+}
